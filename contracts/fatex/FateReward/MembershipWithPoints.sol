ef4d4b8907278037f8712be76adc36887dfc3b99
commonlot <stepcek80@gmail.com>
2021-11-26 01:48:43 +1000

    implement feewithraws, membership points

--- contracts/fatex/FateReward/MembershipWithPoints.sol
+++ contracts/fatex/FateReward/MembershipWithPoints.sol
@@ -6,16 +6,17 @@ import "hardhat/console.sol";
 import "../../libraries/RankedArray.sol";
 
 ////////////////////////////////////////////////////////////////////////////////////////////
-/// @title CWToken
-/// @author MembershipPoint
+/// @title MembershipWithPoints
+/// @author @commonlot
 ////////////////////////////////////////////////////////////////////////////////////////////
-contract MembershipPoints {
+abstract contract MembershipWithPoints {
     uint256 constant public POINTS_PER_BLOCK = 0.08e18;
-    uint256 immutable public EPOCH_END_BLOCK;
+    uint256 public EPOCH_END_BLOCK;
 
     struct MembershipInfo{
-        uint256 depositBlock;
+        uint256 firstDepositBlock; // set when first deposit
         uint256 rankedNumber;
+        uint256 lastWithdrawBlock; // set when first deposit, updates whenever withdraws
     }
 
     // pid => depositors' address array
@@ -23,42 +24,37 @@ contract MembershipPoints {
 
     // pid => address => membershipInfo
     mapping(uint256 => mapping (address => MembershipInfo)) public userMembershipInfo;
-
-    constructor(uint256 epoch_start_block) public {
-        EPOCH_END_BLOCK = epoch_start_block + 8 weeks;
-    }
     
-    /// @notice record deposit block
-    function recordDepositBlock(uint256 _pid, address _user) public {
-        /**
-            TODO: 
-            - if external user calls this, remove _user in param and use msg.sender
-            - if FateRewardController is calling this, add `onlyFateRewardController` logic for validation
-         */
-        require(block.number <= EPOCH_END_BLOCK, "recordDepositBlock: epoch ended");
+    /// @dev record deposit block
+    function recordDepositBlock(uint256 _pid, address _user) internal {
+        uint256 currentBlockNumber = block.number;
+        require(currentBlockNumber <= EPOCH_END_BLOCK, "recordDepositBlock: epoch ended");
+
         uint256 userIndex = _getIndexOfArray(depositedUsers[_pid], _user);
+
         if(userIndex == depositedUsers[_pid].length) {
-            // not recored yet
+            // not recored yet (first deposit)
             depositedUsers[_pid].push(_user);
             userMembershipInfo[_pid][_user] = MembershipInfo({
-                depositBlock: block.number,
-                rankedNumber: 2 ** 256 - 1
+                firstDepositBlock: currentBlockNumber,
+                rankedNumber: 2 ** 256 - 1,
+                lastWithdrawBlock: currentBlockNumber
             });
         }
     }
 
-
-    /// @notice calculate Points earned by this user
-    function userPoints(uint256 _pid, address _user) public view returns (uint256 points){
-        MembershipInfo memory membership = userMembershipInfo[_pid][_user];
-        
-        uint256 currentBlock = block.number;
-        uint256 endBlock = currentBlock > EPOCH_END_BLOCK ? EPOCH_END_BLOCK : currentBlock;
-
-        points = (endBlock - membership.depositBlock) * POINTS_PER_BLOCK;
+    function getEndBlock() internal view returns(uint256 endBlock) {
+        uint256 currentBlockNumber = block.number;
+        endBlock = currentBlockNumber > EPOCH_END_BLOCK ? EPOCH_END_BLOCK : currentBlockNumber;
     }
 
-    /// @notice rank with Points and set rankedNumber to each user
+    /// @dev calculate Points earned by this user
+    function userPoints(uint256 _pid, address _user) public view returns (uint256 points){
+        MembershipInfo memory membership = userMembershipInfo[_pid][_user];
+        points = (getEndBlock() - membership.firstDepositBlock) * POINTS_PER_BLOCK;
+    }
+
+    /// @dev rank with Points and set rankedNumber to each user
     function rank(uint256 _pid) public returns (uint256) {
         address[] memory pool_deposited_user_list = depositedUsers[_pid];
         uint256 deposited_user_counts = pool_deposited_user_list.length;
@@ -76,13 +72,14 @@ contract MembershipPoints {
             MembershipInfo memory membership = userMembershipInfo[_pid][userAddr];
 
             userMembershipInfo[_pid][userAddr] = MembershipInfo({
-                depositBlock: membership.depositBlock,
-                rankedNumber: RankedArray.getIndex(sortedPointsList, pointsList[i])
+                firstDepositBlock: membership.firstDepositBlock,
+                rankedNumber: RankedArray.getIndex(sortedPointsList, pointsList[i]),
+                lastWithdrawBlock: membership.lastWithdrawBlock
             });
         }
     }
 
-    /// @notice library to get index of an array of address list
+    /// @dev library to get index of an array of address list
     function _getIndexOfArray(address[] memory data, address addr) internal pure returns (uint256 index) {
         index = data.length;
         for (uint i=0; i < data.length; i++) {
